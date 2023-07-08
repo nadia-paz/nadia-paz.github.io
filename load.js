b066c5213da8d21d8b5b22e0385898b2de76d46d
Nadia Paz <nadina.abouhamdan@gmail.com>
2023-07-07 21:04:45 -0500

    start loadData()

--- load.js
+++ load.js
@@ -34,3 +34,33 @@ function loadFistItems(){
     container.insertBefore(div, loadButton);
     div.innerHTML = out;
 }
+
+function loadData(){
+    let projects = JSON.parse(localStorage.getItem("projects"));
+    let currentItems = document.querySelectorAll(".x").length;
+    let out = '';
+    let counter = 0;
+    for(let x in projects){
+        if(counter >= currentItems && counter < loadItems + currentItems){
+            let codeBlock = '<div class="work">' +
+                '<img src="' + x.image + '"/>' +
+                '<div class="layer">' +
+                    '<h3>' + x.title + '</h3>' +
+                    '<p>' + x.about + '</p>' +
+                    '<a href="' + x.github + '" target="_blank" rel="noreferrer noopener"><i class="fa-solid fa-arrow-up-right-from-square"></i></a>' +
+                '</div>' +
+            '</div>';
+            out += codeBlock;
+        }
+        counter++;
+    }
+    // create div element
+    let div = document.createElement("div")
+    container.insertBefore(div, loadButton);
+    div.innerHTML = out;
+
+    if(document.querySelectorAll(".x").length = x.length){
+        loadButton.style.display = "none";
+    }
+    fadeIn(div);
+}
